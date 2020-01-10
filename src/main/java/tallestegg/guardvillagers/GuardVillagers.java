bf2d8d9f062c3a9f83a2606c522a455e950b0e62
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-01-09 20:37:44 -0800

    Add files via upload

--- src/main/java/tallestegg/guardvillagers/GuardVillagers.java
+++ src/main/java/tallestegg/guardvillagers/GuardVillagers.java
@@ -1,22 +1,27 @@
 package tallestegg.guardvillagers;
 
 import net.minecraft.block.Blocks;
+
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import tallestegg.guardvillagers.configuration.GuardConfig;
 import tallestegg.guardvillagers.entities.GuardEntity;
 import tallestegg.guardvillagers.renderer.GuardRenderer;
+import tallestegg.guardvillagers.renderer.GuardRenderer2;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -26,7 +31,9 @@ import java.util.stream.Collectors;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("guardvillagers")
 public class GuardVillagers
-{
+{  
+	public static final String MODID = "guardvillagers";
+	
     private static final Logger LOGGER = LogManager.getLogger();
 
     public GuardVillagers() {
@@ -34,6 +41,7 @@ public class GuardVillagers
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, GuardConfig.CLIENT_SPEC);
 
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(HandlerEvents.class);
@@ -45,8 +53,12 @@ public class GuardVillagers
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 
-    private void doClientStuff(final FMLClientSetupEvent event) {
-    	RenderingRegistry.registerEntityRenderingHandler(GuardEntity.class,GuardRenderer::new);
+    private void doClientStuff(final FMLClientSetupEvent event) 
+    {
+    	if (GuardConfig.GuardModel == false)
+    	RenderingRegistry.registerEntityRenderingHandler(GuardEntity.class, GuardRenderer::new);
+    	else
+        RenderingRegistry.registerEntityRenderingHandler(GuardEntity.class, GuardRenderer2::new);
     }
 
     private void enqueueIMC(final InterModEnqueueEvent event)
--- src/main/java/tallestegg/guardvillagers/HandlerEvents.java
+++ src/main/java/tallestegg/guardvillagers/HandlerEvents.java
@@ -1,11 +1,15 @@
 package tallestegg.guardvillagers;
 
 import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.merchant.villager.AbstractVillagerEntity;
 import net.minecraft.entity.monster.AbstractIllagerEntity;
 import net.minecraft.entity.monster.RavagerEntity;
+import net.minecraft.entity.monster.WitchEntity;
 import net.minecraft.entity.monster.ZombieEntity;
+import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import tallestegg.guardvillagers.configuration.GuardConfig;
 import tallestegg.guardvillagers.entities.GuardEntity;
 
 public class HandlerEvents 
@@ -16,8 +20,12 @@ public class HandlerEvents
 		
 	if(event.getEntity() instanceof AbstractIllagerEntity) 
 	{
-		AbstractIllagerEntity monster = (AbstractIllagerEntity)event.getEntity();
-	    monster.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(monster, GuardEntity.class, false));   
+		AbstractIllagerEntity illager = (AbstractIllagerEntity)event.getEntity();
+	    illager.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(illager, GuardEntity.class, false));   
+	    if (GuardConfig.RaidAnimals == true)
+	    if (illager.isRaidActive()) {
+	    	illager.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(illager, AnimalEntity.class, false));   
+	    }
 	}
 	
 	if(event.getEntity() instanceof ZombieEntity) 
@@ -28,7 +36,23 @@ public class HandlerEvents
 	if(event.getEntity() instanceof RavagerEntity) 
 	{
 		RavagerEntity ravager = (RavagerEntity)event.getEntity();
-	    ravager.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(ravager, GuardEntity.class, false));   
+	    ravager.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(ravager, GuardEntity.class, false));  
+	    if (GuardConfig.RaidAnimals == true)
+	    if (ravager.isRaidActive()) {
+	    	ravager.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(ravager, AnimalEntity.class, false));   
+	    }
 	}
+	if(event.getEntity() instanceof WitchEntity) 
+	{
+		WitchEntity witch = (WitchEntity)event.getEntity();
+	    witch.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(witch, GuardEntity.class, false));   
+	    if (GuardConfig.WitchesVillager == true)
+	    witch.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(witch, AbstractVillagerEntity.class, false));   
+	    if (GuardConfig.RaidAnimals == true)
+	    if (witch.isRaidActive()) {
+	    	witch.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(witch, AnimalEntity.class, false));   
+	    }
+	}
+	
   }
 }
