f59a62bd9a0e1050e87cc154f4eb176d4e10651d
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-02-08 21:04:51 -0800

    Add files via upload

--- src/main/java/tallestegg/guardvillagers/entities/GuardEntity.java
+++ src/main/java/tallestegg/guardvillagers/entities/GuardEntity.java
@@ -8,6 +8,7 @@ import net.minecraft.entity.ILivingEntityData;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.ai.goal.AvoidEntityGoal;
 import net.minecraft.entity.ai.goal.HurtByTargetGoal;
 import net.minecraft.entity.ai.goal.LookAtGoal;
 import net.minecraft.entity.ai.goal.LookRandomlyGoal;
@@ -108,6 +109,7 @@ public class GuardEntity extends CreatureEntity
 	      this.goalSelector.addGoal(8, new RandomWalkingGoal(this, 0.6D));
 	      this.goalSelector.addGoal(8, new OpenDoorGoal(this, true));
 	      this.targetSelector.addGoal(2, new HurtByTargetGoal(this));
+	      this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, RavagerEntity.class, 12.0F, 0.5D, 0.5D));
 	      this.goalSelector.addGoal(10, new LookAtGoal(this, MobEntity.class, 8.0F));
 	      this.goalSelector.addGoal(10, new LookAtGoal(this, PlayerEntity.class, 8.0F));
 	      this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, ZombieEntity.class, true));
