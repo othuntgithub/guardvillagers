c9e378c481d273b7f4ac814d8500d974e5dc6d82
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-02-17 09:05:31 -0800

    fixed #1, now you have to shift right click

---
+++ src/main/java/tallestegg/guardvillagers/VillagerToGuard.java
@@ -0,0 +1,55 @@
+package tallestegg.guardvillagers;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.merchant.villager.VillagerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import tallestegg.guardvillagers.entities.GuardEntity;
+
+@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+public class VillagerToGuard 
+{
+	@SubscribeEvent
+	  public void onEntityInteract(PlayerInteractEvent.EntityInteract e) 
+	    {
+	     if ((e.getWorld()).isRemote) 
+	    {
+	      return;
+	    }
+	     ItemStack itemstack = e.getItemStack();
+	     if (itemstack.getItem() instanceof SwordItem && e.getPlayer().isSneaking()) 
+	     {
+	       Entity target = e.getTarget();
+	       if ((target instanceof VillagerEntity)) 
+	       {
+	         VillagerEntity villager = (VillagerEntity) e.getTarget();
+	         this.VillagerConvert(villager, e);
+	         itemstack.shrink(1);
+	       } 
+	} 
+  }
+
+	private void VillagerConvert(LivingEntity entity, PlayerInteractEvent.EntityInteract e) 
+	{
+		  if (entity instanceof VillagerEntity);
+		    ItemStack itemstack = e.getItemStack();
+		    GuardEntity guard = GuardEntityType.GUARD.create(entity.world);
+		    VillagerEntity villager = (VillagerEntity)entity;
+		    guard.copyLocationAndAnglesFrom(villager);
+		    guard.setItemStackToSlot(EquipmentSlotType.MAINHAND, itemstack.copy());
+		    int i = guard.getRandomTypeForBiome(guard.world);
+		    guard.setGuardVariant(i);
+		    if (villager.hasCustomName()) 
+		    {
+		      guard.setCustomName(villager.getCustomName());
+		      guard.setCustomNameVisible(villager.isCustomNameVisible());
+		    }
+		    villager.world.addEntity(guard);
+	        villager.remove();	 
+	} 
+}
