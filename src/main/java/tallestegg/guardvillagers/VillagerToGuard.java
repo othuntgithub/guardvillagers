d2e7fbd52a9a0218fa3df607ba076433eebe1e86
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-03-10 18:09:42 -0700

    more archer preparations

--- src/main/java/tallestegg/guardvillagers/VillagerToGuard.java
+++ src/main/java/tallestegg/guardvillagers/VillagerToGuard.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.merchant.villager.VillagerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ShootableItem;
 import net.minecraft.item.SwordItem;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -18,7 +19,7 @@ public class VillagerToGuard
 	  public void onEntityInteract(PlayerInteractEvent.EntityInteract e) 
 	    {
 	     ItemStack itemstack = e.getItemStack();
-	     if (itemstack.getItem() instanceof SwordItem && e.getPlayer().isShiftKeyDown()) 
+	     if (itemstack.getItem() instanceof SwordItem || itemstack.getItem() instanceof ShootableItem && e.getPlayer().isShiftKeyDown()) 
 	     {
 	       Entity target = e.getTarget();
 	       if ((target instanceof VillagerEntity)) 
