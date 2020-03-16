af87294dd58333afc2437ebc227be629fa6048aa
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-03-15 21:10:31 -0700

    ranged guards for 1.14

--- src/main/java/tallestegg/guardvillagers/entities/GuardEntity.java
+++ src/main/java/tallestegg/guardvillagers/entities/GuardEntity.java
@@ -1,98 +1,111 @@
 package tallestegg.guardvillagers.entities;
 
+import java.util.function.Predicate;
+
 import javax.annotation.Nullable;
 
-import net.minecraft.entity.CreatureEntity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ILivingEntityData;
-import net.minecraft.entity.MobEntity;
-import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.SpawnReason;
-import net.minecraft.entity.ai.goal.AvoidEntityGoal;
-import net.minecraft.entity.ai.goal.HurtByTargetGoal;
-import net.minecraft.entity.ai.goal.LookAtGoal;
-import net.minecraft.entity.ai.goal.LookRandomlyGoal;
-import net.minecraft.entity.ai.goal.MeleeAttackGoal;
-import net.minecraft.entity.ai.goal.MoveTowardsTargetGoal;
-import net.minecraft.entity.ai.goal.MoveTowardsVillageGoal;
-import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
-import net.minecraft.entity.ai.goal.OpenDoorGoal;
-import net.minecraft.entity.ai.goal.RandomWalkingGoal;
-import net.minecraft.entity.ai.goal.SwimGoal;
-import net.minecraft.entity.monster.AbstractIllagerEntity;
-import net.minecraft.entity.monster.IMob;
-import net.minecraft.entity.monster.IllusionerEntity;
-import net.minecraft.entity.monster.RavagerEntity;
-import net.minecraft.entity.monster.VexEntity;
-import net.minecraft.entity.monster.WitchEntity;
-import net.minecraft.entity.monster.ZombieEntity;
+import net.minecraft.client.renderer.*;
+import net.minecraft.entity.*;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.monster.*;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileHelper;
 import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.item.ShootableItem;
-import net.minecraft.item.SwordItem;
+import net.minecraft.item.*;
 import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.network.datasync.DataParameter;
-import net.minecraft.network.datasync.DataSerializers;
-import net.minecraft.network.datasync.EntityDataManager;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.Hand;
-import net.minecraft.util.SoundEvent;
-import net.minecraft.util.SoundEvents;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.DifficultyInstance;
-import net.minecraft.world.IWorld;
-import net.minecraft.world.World;
+import net.minecraft.network.datasync.*;
+import net.minecraft.util.*;
+import net.minecraft.util.math.*;
+import net.minecraft.world.*;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biome.Category;
 import tallestegg.guardvillagers.configuration.GuardConfig;
+import tallestegg.guardvillagers.entities.goals.DefendVillageGuardGoal;
+import tallestegg.guardvillagers.entities.goals.RangedCrossbowAttackPassiveGoal;
 
-public class GuardEntity extends CreatureEntity
-{
+public class GuardEntity extends CreatureEntity implements ICrossbowUser, IRangedAttackMob
+{ 
 	private static final DataParameter<Integer> GUARD_VARIANT = EntityDataManager.createKey(GuardEntity.class, DataSerializers.VARINT);
-	
+	private static final DataParameter<Boolean> DATA_CHARGING_STATE = EntityDataManager.createKey(GuardEntity.class, DataSerializers.BOOLEAN);
+    private final MeleeAttackGoal aiAttackOnCollide = new MeleeAttackGoal(this, 1.0D, true) 
+    {
+        /**
+         * Reset the task's internal state. Called when this task is interrupted by another one
+         */
+        public void resetTask() {
+            super.resetTask();
+            GuardEntity.this.setAggroed(false);
+        }
+
+        /**
+         * Execute a one shot task or start executing a continuous task
+         */
+        public void startExecuting() {
+            super.startExecuting();
+            GuardEntity.this.setAggroed(true);
+        }
+    };
+    private final RangedCrossbowAttackPassiveGoal<GuardEntity> aiCrossBowAttack = new RangedCrossbowAttackPassiveGoal<GuardEntity>(this, 1.0D, 8.0F);
+	 
 	public GuardEntity(EntityType<? extends GuardEntity> type, World world)
 	{
 		super(type, world);
-		enablePersistence();
+		this.enablePersistence();
+		this.setCombatTask();
 	}
 
 	@Override
 	public ILivingEntityData onInitialSpawn(IWorld worldIn, DifficultyInstance difficultyIn, SpawnReason reason, @Nullable ILivingEntityData spawnDataIn, @Nullable CompoundNBT dataTag) 
 	{
-	 int type = GuardEntity.getRandomTypeForBiome(worldIn, this.getPosition());
+	  int type = GuardEntity.getRandomTypeForBiome(world, this.getPosition());
 	  if (spawnDataIn instanceof GuardEntity.GuardData) 
 	  {
         type = ((GuardEntity.GuardData)spawnDataIn).variantData;
 	    spawnDataIn = new GuardEntity.GuardData(type);
 	  }
-		
+       this.setCombatTask();
 	   this.setGuardVariant(type);
 	   this.setEquipmentBasedOnDifficulty(difficultyIn);
 	   return super.onInitialSpawn(worldIn, difficultyIn, reason, spawnDataIn, dataTag);
 	}
 	
-	   protected SoundEvent getAmbientSound() 
-	   {
-	     return SoundEvents.ENTITY_VILLAGER_AMBIENT;
-	   }
+	public void setCombatTask() {
+        if (this.world != null && !this.world.isRemote) {
+            this.goalSelector.removeGoal(this.aiAttackOnCollide);
+            this.goalSelector.removeGoal(this.aiCrossBowAttack);
 
-	   protected SoundEvent getHurtSound(DamageSource damageSourceIn) 
-	   {
-	      return SoundEvents.ENTITY_VILLAGER_HURT;
-	   }
 
-	   protected SoundEvent getDeathSound() 
-	   {
-	      return SoundEvents.ENTITY_VILLAGER_DEATH;
-	   }
+            ItemStack itemstack2 = this.getHeldItem(ProjectileHelper.getHandWith(this, Items.CROSSBOW));
+            if (itemstack2.getItem() instanceof net.minecraft.item.CrossbowItem) {
+                this.targetSelector.addGoal(4, this.aiCrossBowAttack);
+            } else {
+            	 this.targetSelector.addGoal(4, this.aiAttackOnCollide);
+            }
+
+        }
+    }
+	
+	protected SoundEvent getAmbientSound() 
+	{
+	  return SoundEvents.ENTITY_VILLAGER_AMBIENT;
+	}
+
+    protected SoundEvent getHurtSound(DamageSource damageSourceIn) 
+	{
+	  return SoundEvents.ENTITY_VILLAGER_HURT;
+	}
+
+	protected SoundEvent getDeathSound() 
+	{
+	  return SoundEvents.ENTITY_VILLAGER_DEATH;
+	}
 	   
 	@Override
 	public void readAdditional(CompoundNBT compound) 
 	{
 		super.readAdditional(compound);
 		this.setGuardVariant(compound.getInt("Type"));
+		this.setCombatTask();
 	}
 	
 	
@@ -100,6 +113,7 @@ public class GuardEntity extends CreatureEntity
 	{
 	    this.updateArmSwingProgress();
 	    super.livingTick();
+	   
 	}
 	
 	@Override
@@ -107,8 +121,19 @@ public class GuardEntity extends CreatureEntity
 	{
 		super.registerData();
 		this.dataManager.register(GUARD_VARIANT, 0);
+		this.dataManager.register(DATA_CHARGING_STATE, false);
 	}
-	
+
+    public boolean isCharging() 
+    {
+	  return this.dataManager.get(DATA_CHARGING_STATE);
+    }
+
+    public void setCharging(boolean p_213671_1_) 
+    {
+	   this.dataManager.set(DATA_CHARGING_STATE, p_213671_1_);
+    }
+	   
 	protected void setEquipmentBasedOnDifficulty(DifficultyInstance difficulty) 
 	{
 	  int i = this.rand.nextInt(2);
@@ -122,7 +147,6 @@ public class GuardEntity extends CreatureEntity
 	     }
 	  this.inventoryHandsDropChances[EquipmentSlotType.MAINHAND.getIndex()] = 50.0F;
 	}
-	
 	
 	public int getGuardVariant()
     {
@@ -138,25 +162,24 @@ public class GuardEntity extends CreatureEntity
 	{
 		  this.goalSelector.addGoal(1, new SwimGoal(this));
 	      this.goalSelector.addGoal(6, new LookRandomlyGoal(this));
-	      this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.0D, true));
 	      this.goalSelector.addGoal(2, new MoveTowardsTargetGoal(this, 0.9D, 32.0F));
 	      this.goalSelector.addGoal(2, new MoveTowardsVillageGoal(this, 0.6D));
 	      this.goalSelector.addGoal(8, new RandomWalkingGoal(this, 0.6D));
+	      this.goalSelector.addGoal(2, new DefendVillageGuardGoal(this));
 	      this.goalSelector.addGoal(1, new OpenDoorGoal(this, true));
-	      this.goalSelector.addGoal(8, new AvoidEntityGoal<RavagerEntity>(this, RavagerEntity.class, 12.0F, 0.5D, 0.5D){
+	      this.goalSelector.addGoal(8, new AvoidEntityGoal<RavagerEntity>(this, RavagerEntity.class, 12.0F, 0.5D, 0.5D) {
 				@Override
 				public boolean shouldExecute() {
-					return ((GuardEntity)this.entity).getHealth() <= 13 && super.shouldExecute();
+					return ((GuardEntity)this.entity).getHealth() <= 15 && super.shouldExecute();
 				}
 				
-			});
-	      
+			});	      
 	      this.goalSelector.addGoal(10, new LookAtGoal(this, MobEntity.class, 8.0F));
 	      this.goalSelector.addGoal(10, new LookAtGoal(this, PlayerEntity.class, 8.0F));
 	      this.targetSelector.addGoal(5, new NearestAttackableTargetGoal<RavagerEntity>(this, RavagerEntity.class, true) {
 				@Override
 				public boolean shouldExecute() {
-					return ((GuardEntity)this.goalOwner).getHealth() >= 13 && super.shouldExecute();
+					return ((GuardEntity)this.goalOwner).getHealth() >= 15 && super.shouldExecute();
 				}
 				
 			});
@@ -176,6 +199,75 @@ public class GuardEntity extends CreatureEntity
 	      }
 	 }
 	
+	@Override
+	public void attackEntityWithRangedAttack(LivingEntity target, float distanceFactor) 
+	{
+		Hand hand = ProjectileHelper.getHandWith(this, Items.CROSSBOW);
+	      ItemStack itemstack = this.getHeldItem(hand);
+	      if (this.isHolding(Items.CROSSBOW)) {
+	         CrossbowItem.fireProjectiles(this.world, this, hand, itemstack, 1.6F, (float)(14 - this.world.getDifficulty().getId() * 4));
+	      }
+
+	      this.idleTime = 0;
+	}
+
+	
+   @Override
+   public void setItemStackToSlot(EquipmentSlotType slotIn, ItemStack stack)
+   {
+	  super.setItemStackToSlot(slotIn, stack);
+	  if (!this.world.isRemote) 
+	  {
+		this.setCombatTask();
+	  }
+    }
+   
+   
+	@Override
+	public void shoot(LivingEntity target, ItemStack p_213670_2_, IProjectile projectile, float projectileAngle) 
+	{
+	      Entity entity = (Entity)projectile;
+	      double d0 = target.posX - this.posX;
+	      double d1 = target.posZ - this.posZ;
+	      double d2 = (double)MathHelper.sqrt(d0 * d0 + d1 * d1);
+	      double d3 = target.getBoundingBox().minY + (double)(target.getHeight() / 3.0F) - entity.posY + d2 * (double)0.2F;
+	      Vector3f vector3f = this.func_213673_a(new Vec3d(d0, d3, d1), projectileAngle);
+	      projectile.shoot((double)vector3f.getX(), (double)vector3f.getY(), (double)vector3f.getZ(), 1.6F, (float)(14 - this.world.getDifficulty().getId() * 4));
+	      this.playSound(SoundEvents.ITEM_CROSSBOW_SHOOT, 1.0F, 1.0F / (this.getRNG().nextFloat() * 0.4F + 0.8F));
+	}
+	
+	 private Vector3f func_213673_a(Vec3d p_213673_1_, float p_213673_2_) {
+	      Vec3d vec3d = p_213673_1_.normalize();
+	      Vec3d vec3d1 = vec3d.crossProduct(new Vec3d(0.0D, 1.0D, 0.0D));
+	      if (vec3d1.lengthSquared() <= 1.0E-7D) {
+	         vec3d1 = vec3d.crossProduct(this.getUpVector(1.0F));
+	      }
+
+	      Quaternion quaternion = new Quaternion(new Vector3f(vec3d1), 90.0F, true);
+	      Vector3f vector3f = new Vector3f(vec3d);
+	      vector3f.func_214905_a(quaternion);
+	      Quaternion quaternion1 = new Quaternion(vector3f, p_213673_2_, true);
+	      Vector3f vector3f1 = new Vector3f(vec3d);
+	      vector3f1.func_214905_a(quaternion1);
+	      return vector3f1;
+	   }
+
+	
+	
+	 @Override
+	 public ItemStack findAmmo(ItemStack shootable) 
+	 {
+	      if (shootable.getItem() instanceof ShootableItem) 
+	      {
+	         Predicate<ItemStack> predicate = ((ShootableItem)shootable.getItem()).getAmmoPredicate();
+	         ItemStack itemstack = ShootableItem.getHeldAmmo(this, predicate);
+	         return itemstack.isEmpty() ? new ItemStack(Items.ARROW) : itemstack;
+	      }  
+	       else 
+	       {
+	         return ItemStack.EMPTY;
+	       }
+	   }
 	
 	@Override
 	public void writeAdditional(CompoundNBT compound) 
@@ -185,59 +277,79 @@ public class GuardEntity extends CreatureEntity
 	}
 	
 	public static int getRandomTypeForBiome(IWorld world, BlockPos pos) {
-	    Biome biome = world.getBiome(pos);
-		if(biome.getCategory() == Category.PLAINS) 
-		{
-				return 0;
-		}
-		
-		if (biome.getCategory() == Category.DESERT) 
-		{
-				return 1;
-		}
-		
-		if (biome.getCategory() == Category.SAVANNA) 
-		{
-				return 2;
-		}
-		
-		if (biome.getCategory() == Category.SWAMP) 
-		{
-				return 3;
-		}
-		
-		if (biome.getCategory() == Category.JUNGLE) 
-		{
-				return 4;
-		}
-		
-		if (biome.getCategory() == Category.TAIGA) 
-		{
-				return 5;
-		}
-		
-		if (biome.getCategory() == Category.ICY) 
-		{
-				return 6;
-		}
-		else 
-		{
-		  return 0;
-		}
-}
+		    Biome biome = world.getBiome(pos);
+			if(biome.getCategory() == Category.PLAINS) 
+			{
+					return 0;
+			}
+			
+			if (biome.getCategory() == Category.DESERT) 
+			{
+					return 1;
+			}
+			
+			if (biome.getCategory() == Category.SAVANNA) 
+			{
+					return 2;
+			}
+			
+			if (biome.getCategory() == Category.SWAMP) 
+			{
+					return 3;
+			}
+			
+			if (biome.getCategory() == Category.JUNGLE) 
+			{
+					return 4;
+			}
+			
+			if (biome.getCategory() == Category.TAIGA) 
+			{
+					return 5;
+			}
+			
+			if (biome.getCategory() == Category.ICY) 
+			{
+					return 6;
+			}
+			else 
+			{
+			  return 0;
+			}
+	}
 	
+	 public GuardEntity.ArmPose getArmPose() {
+	      /*if (this.isCharging()) 
+	      {
+	        return GuardEntity.ArmPose.CROSSBOW_CHARGE;
+	      }*/
+	      /*else*/ if (this.isHolding(Items.CROSSBOW)) {
+	            return GuardEntity.ArmPose.CROSSBOW_HOLD;
+	     } else {
+          return this.isAggressive() ? GuardEntity.ArmPose.NEUTRAL : GuardEntity.ArmPose.NEUTRAL;
+	     }
+		  
+	   }
+
+	   public void updateRidden() {
+		      super.updateRidden();
+		      if (this.getRidingEntity() instanceof CreatureEntity) {
+		         CreatureEntity creatureentity = (CreatureEntity)this.getRidingEntity();
+		         this.renderYawOffset = creatureentity.renderYawOffset;
+		      }
+
+		   }
 	public boolean processInteract(PlayerEntity player, Hand hand)
 	{
 	        ItemStack heldStack = player.getHeldItem(hand);
-	        if (heldStack.getItem() instanceof SwordItem || heldStack.getItem() instanceof ShootableItem && player.isSneaking())
+	        if (heldStack.getItem() instanceof SwordItem || heldStack.getItem() instanceof CrossbowItem && player.isSneaking())
 	        {
 	            this.setItemStackToSlot(EquipmentSlotType.MAINHAND, heldStack.copy());
 	            if (!player.abilities.isCreativeMode)
 	                heldStack.shrink(1);
+	            }
+			    return true;
 	        }
-			return true;
-	    }
-	
 	public static String getNameByType(int id) 
 	{
 		switch(id) 
@@ -260,6 +372,7 @@ public class GuardEntity extends CreatureEntity
 		return "";
 	}
 	
+	
 	protected void registerAttributes() 
 	{
 	      super.registerAttributes();
@@ -280,6 +393,13 @@ public class GuardEntity extends CreatureEntity
 		}
 	}
 	
+	 public static enum ArmPose {
+	      BOW_AND_ARROW, //Maybe?
+	      CROSSBOW_HOLD,
+	      CROSSBOW_CHARGE,
+	      NEUTRAL;
+	   }
+
 	/**
 	 * Credit - SmellyModder for Biome Specific Textures
 	 */
