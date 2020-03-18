47977d635286d4f34b631d551b8251f1827652ed
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-03-17 22:05:02 -0700

    new guard renderer

--- src/main/java/tallestegg/guardvillagers/renderer/GuardRenderer.java
+++ src/main/java/tallestegg/guardvillagers/renderer/GuardRenderer.java
@@ -4,6 +4,7 @@ import javax.annotation.Nullable;
 
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.layers.HeadLayer;
 import net.minecraft.client.renderer.entity.layers.HeldItemLayer;
 import net.minecraft.util.ResourceLocation;
 import tallestegg.guardvillagers.GuardVillagers;
@@ -11,13 +12,14 @@ import tallestegg.guardvillagers.entities.GuardEntity;
 import tallestegg.guardvillagers.models.GuardModel;
 
 
-public class GuardRenderer extends MobRenderer<GuardEntity, GuardModel<GuardEntity>>
+public class GuardRenderer extends MobRenderer<GuardEntity, GuardModel>
 {
 
     public GuardRenderer(EntityRendererManager manager) 
     {
-        super(manager, new GuardModel<GuardEntity>(), 0.5f);
+        super(manager, new GuardModel(), 0.5f);
         this.addLayer(new HeldItemLayer<>(this));
+        this.addLayer(new HeadLayer<>(this));
     }
 
     @Nullable
