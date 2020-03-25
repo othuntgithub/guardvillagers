d77d5db84523bdaaa74556ed825585c091e2b663
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-03-25 12:39:11 -0700

    fixed somtthing

--- src/main/java/tallestegg/guardvillagers/entities/GuardEntity.java
+++ src/main/java/tallestegg/guardvillagers/entities/GuardEntity.java
@@ -349,7 +349,7 @@ public class GuardEntity extends CreatureEntity implements ICrossbowUser, IRange
 	public boolean processInteract(PlayerEntity player, Hand hand)
 	{
 	        ItemStack heldStack = player.getHeldItem(hand);
-	        if (heldStack.getItem() instanceof SwordItem || heldStack.getItem() instanceof CrossbowItem && player.isShiftKeyDown())
+	        if (heldStack.getItem() instanceof SwordItem && player.isShiftKeyDown() || heldStack.getItem() instanceof CrossbowItem && player.isShiftKeyDown())
 	        {
 	            this.setItemStackToSlot(EquipmentSlotType.MAINHAND, heldStack.copy());
 	            if (!player.abilities.isCreativeMode)
