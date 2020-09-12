48866e37be57c48fa9e07329f956f8bf9039f8e1
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-09-12 16:59:10 -0700

    fixed spawn egg colors

--- src/main/java/tallestegg/guardvillagers/GuardVillagers.java
+++ src/main/java/tallestegg/guardvillagers/GuardVillagers.java
@@ -22,15 +22,14 @@ import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import tallestegg.guardvillagers.configuration.GuardConfig;
 import tallestegg.guardvillagers.entities.GuardEntity;
+import tallestegg.guardvillagers.items.DeferredSpawnEggItem;
 import tallestegg.guardvillagers.renderer.GuardRenderer;
 
 @Mod(GuardVillagers.MODID)
-public class GuardVillagers
-{  
-	public static final String MODID = "guardvillagers";
+public class GuardVillagers {
+    public static final String MODID = "guardvillagers";
 
-    public GuardVillagers() 
-    {
+    public GuardVillagers() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
@@ -39,60 +38,48 @@ public class GuardVillagers
 
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(new HandlerEvents());
-		MinecraftForge.EVENT_BUS.register(new VillagerToGuard());
-		GuardEntityType.ENTITIES.register(FMLJavaModLoadingContext.get().getModEventBus());
-		GuardItems.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-		GuardSpawner.inject();
+        MinecraftForge.EVENT_BUS.register(new VillagerToGuard());
+        GuardEntityType.ENTITIES.register(FMLJavaModLoadingContext.get().getModEventBus());
+        GuardItems.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        GuardSpawner.inject();
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
-       if (GuardConfig.IllusionerRaids) {
-	     Raid.WaveMember.create("thebluemengroup", EntityType.ILLUSIONER, new int[]{0, 0, 0, 0, 0, 1, 1, 2});
-       }
+    private void setup(final FMLCommonSetupEvent event) {
+        if (GuardConfig.IllusionerRaids) {
+            Raid.WaveMember.create("thebluemengroup", EntityType.ILLUSIONER, new int[] { 0, 0, 0, 0, 0, 1, 1, 2 });
+        }
     }
 
-    private void doClientStuff(final FMLClientSetupEvent event) 
-    {
-      RenderingRegistry.registerEntityRenderingHandler(GuardEntityType.GUARD.get(), GuardRenderer::new);
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        RenderingRegistry.registerEntityRenderingHandler(GuardEntityType.GUARD.get(), GuardRenderer::new);
     }
 
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-    	
-    }
-
-    private void processIMC(final InterModProcessEvent event)
-    {
+    private void enqueueIMC(final InterModEnqueueEvent event) {
 
     }
+
+    private void processIMC(final InterModProcessEvent event) {
+
+    }
+
     @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event)
-    {
-        
+    public void onServerStarting(FMLServerStartingEvent event) {
+
     }
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents 
-    {
-    	@SubscribeEvent
-        public static void registerSpawnEggs(final RegistryEvent.Register<Item> event) 
-    	{
-    		 event.getRegistry().registerAll
-    		 (
-    	    	new SpawnEggItem(EntityType.ILLUSIONER, 9804699, 4547222, new Item.Properties().group(ItemGroup.MISC)).setRegistryName(GuardVillagers.MODID, "illusioner_spawn_egg"),
-    	    	new SpawnEggItem(EntityType.IRON_GOLEM, 12960449, 16769484, new Item.Properties().group(ItemGroup.MISC)).setRegistryName(GuardVillagers.MODID, "iron_golem_spawn_egg"),
-    	        new SpawnEggItem(EntityType.SNOW_GOLEM, 15663103, 16753185, new Item.Properties().group(ItemGroup.MISC)).setRegistryName(GuardVillagers.MODID, "snow_golem_spawn_egg")   		
-    		 );
-    	 }
-    	
-    	@SubscribeEvent(priority = EventPriority.LOWEST)
-        public static void imstuff(final RegistryEvent.Register<EntityType<?>> event) 
-    	{
-    		GlobalEntityTypeAttributes.put(GuardEntityType.GUARD.get(), GuardEntity.func_234200_m_().func_233813_a_());
-    	}
+
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void registerSpawnEggs(final RegistryEvent.Register<Item> event) {
+            event.getRegistry().registerAll(new SpawnEggItem(EntityType.ILLUSIONER, 9804699, 4547222, new Item.Properties().group(ItemGroup.MISC)).setRegistryName(GuardVillagers.MODID, "illusioner_spawn_egg"),
+                    new SpawnEggItem(EntityType.IRON_GOLEM, 12960449, 16769484, new Item.Properties().group(ItemGroup.MISC)).setRegistryName(GuardVillagers.MODID, "iron_golem_spawn_egg"),
+                    new SpawnEggItem(EntityType.SNOW_GOLEM, 15663103, 16753185, new Item.Properties().group(ItemGroup.MISC)).setRegistryName(GuardVillagers.MODID, "snow_golem_spawn_egg"));
+        }
+
+        @SubscribeEvent(priority = EventPriority.LOWEST)
+        public static void imstuff(final RegistryEvent.Register<EntityType<?>> event) {
+            GlobalEntityTypeAttributes.put(GuardEntityType.GUARD.get(), GuardEntity.func_234200_m_().func_233813_a_());
+            DeferredSpawnEggItem.initUnaddedEggs(); //credit : Cadiboo
+        }
     }
 }
-
-
-
-
